31c9fabeab493bb37eb990b09f9c5c5d0fd42927
Wasu วัชรากูล <67475566+Wasuthoo@users.noreply.github.com>
2022-04-11 15:41:46 +0000

    Update Replit Fixed className

--- pages/index.js
+++ pages/index.js
@@ -40,7 +40,7 @@ export default function Home() {
                   
                   <div className="flex justify-center mt-10">
                   <div className=" mb-3 xl:w-96">
-                    <select class="form-select appearance-none block w-full px-3 py-1.5  text-base font-normal text-gray-700 bg-white bg-clip-padding bg-no-repeat
+                    <select className="form-select appearance-none block w-full px-3 py-1.5  text-base font-normal text-gray-700 bg-white bg-clip-padding bg-no-repeat
                       border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" aria-label="Default select example">
                         <option value>ระบุภาค</option>
                         <option value="1">ภาคเหนือ</option>
