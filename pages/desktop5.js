73a3f65cd35d3837bd62769073c36adc149e67a1
Wasu วัชรากูล <67475566+Wasuthoo@users.noreply.github.com>
2022-04-14 03:12:57 +0000

    Replit V2

---
+++ pages/desktop5.js
@@ -0,0 +1,61 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import Navbar from '../components/Navbar'
+
+export default function Home() {
+  return (
+  <div>
+    <h1 className="text-3xl font-bold text-indigo-900 ml-5  p-5 py-8 ">การกระจายเชิงภูมิภาค</h1>
+      <div className="mb-8">       
+        <div className="flex justify-left mt-10">
+          <div className=" mb-3 xl:w-96">
+            <select className="form-select appearance-none block w-full px-3 py-1.5  text-base font-normal text-gray-700 bg-white bg-clip-padding bg-no-repeat  
+                      border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" aria-label="Default select example">
+              <option value>ระบุภาค</option>
+              <option value="1">ภาคเหนือ</option>
+              <option value="2">ภาคกลาง</option>
+              <option value="3">ภาคตะวันออก</option>
+              <option value="4">ตะวันออกเฉียงเหนือ</option>
+              <option value="5">ภาคตะวันตก</option>
+              <option value="6">ภาคใต้</option>
+              <option value="7">ดูทั้งหมด</option>
+            </select>
+          </div>
+        </div>
+      </div> 
+      <div class="flex justify-center item-center">
+        <table class="shadow-2xl font-bold border-2 border-cyan-200 w-6/12">
+          <thead class="text-cyan">
+            <tr class="bg-white cursor-pointer  duration-300">
+              <th class="py-3 bg-white-800">ลำดับที่</th>
+              <th class="py-3 bg-white-800">ชื่อ</th>
+              <th class="py-3 bg-white-800">นามสกุล</th>
+              <th class="py-3 bg-white-800">รหัสนักศึกษา</th>
+            </tr>
+          </thead>
+          <tbody class="text-black text-center">
+            <tr class="bg-cyan-200 cursor-pointer  duration-300">
+              <td class="py-3 px-6">1</td>
+              <td class="py-3 px-6"> </td>
+              <td class="py-3 px-6"> </td>
+              <td class="py-3 px-6"> </td>
+            </tr>
+            <tr class="bg-cyan-300 cursor-pointer  duration-300">
+              <td class="py-3 px-6">2</td>
+              <td class="py-3 px-6"> </td>
+              <td class="py-3 px-6"> </td>
+              <td class="py-3 px-6"> </td>
+            </tr>
+            <tr class="bg-cyan-400 cursor-pointer  duration-300">
+              <td class="py-3 px-6">3</td>
+              <td class="py-3 px-6"> </td>
+              <td class="py-3 px-6"> </td>
+              <td class="py-3 px-6"> </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+  </div>
+  )
+}
